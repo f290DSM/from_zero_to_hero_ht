366b501b9061c784670881422130cb774fdf0f17
BZR4 <BZR4@users.noreply.github.com>
2024-03-17 18:10:33 -0300

    Update

--- lib/features/products/presentation/categories_page.dart
+++ lib/features/products/presentation/categories_page.dart
@@ -1,8 +1,4 @@
-import 'dart:math';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:from_zero_to_hero_ht/features/products/presentation/providers/category_provider.dart';
 
